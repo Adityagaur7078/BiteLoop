c1cbbf4984c7a57cc1db34540d29c7417ac5bddf
Adityagaur7078 <adityagaur7078@gmail.com>
2026-07-23 17:16:29 +0530

    fix: remove DNS server configuration from server.js

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,6 @@ require('dotenv').config();
 const app = require('./src/app');
 const connectDB = require('./src/db/db');
 
-const dns = require("node:dns");
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
-
-
 const PORT = process.env.PORT || 3000;
 
 async function startServer() {
